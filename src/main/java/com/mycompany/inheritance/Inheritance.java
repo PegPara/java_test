f301240c823ad78e5d7af99915f087a9f862ebb6
Peggy Para <Peggy Para@Peggy_2004>
2025-01-07 19:04:42 +0200

    my first program

---
+++ src/main/java/com/mycompany/inheritance/Inheritance.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inheritance;
+
+/**
+ *
+ * @author Peggy Para
+ */
+public class Inheritance {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
